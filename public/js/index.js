8fc826919a7c459974a4ae05fedab3d641febeb5
DESKTOP-O0ACO2G\Nata <asd>
2018-04-20 16:26:08 +0300

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to server');
 
-	socket.emit('createMessage', {
-		from: 'Pavlo',
-		text: 'Hello!'
-	});
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', {
-		from: 'Jan',
-		text: 'Hey! It\'s',
-		createAt: new Date().getTime()
-	});
-
 	socket.on('createMessage', (message) => {
 		console.log(message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {
